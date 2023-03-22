32c78b62e0d4b7c709b31d99df902d8247c5a31e
Jesse Krensel <jdkrensel@gmail.com>
2023-03-22 00:10:11 -0400

    add comment

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,3 +1,10 @@
+/**
+ * Please notice the Layout component does not render the BestAudio component intentially.
+ * The BestAudio components fetches images from a headless CMS using the function getStaticProps(), which can only be called from within a page as of March 2023.
+ * For performance reasons (pre-rendering, SEO), BestAudio must be rendered and getStaticProps() called on a page-by-page basis.
+ *
+ */
+
 import { Box } from "@mui/material";
 
 import Navbar from "@/components/layout/Navbar";
@@ -5,7 +12,7 @@ import Footer from "@/components/layout/Footer";
 
 import logo from "/public/shared-desktop-logo.svg";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { theme } from "@/styles/theme"
+import { theme } from "@/styles/theme";
 
 export default function Layout({ children }) {
     return (
