2c3c4acc614e49b61577f39fcafe30a2b7e0c334
Jesse Krensel <jdkrensel@gmail.com>
2023-03-24 13:21:00 -0400

    finish static component – add images, styling, and map()

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,102 +6,116 @@ import {
   Container,
   CardHeader,
   CardContent,
+  CardActionArea,
+  Typography,
+  Button,
 } from "@mui/material";
 import Image from "next/image";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export default function Gallery({ assets }) {
   const findAssetById = (assets, id) => assets.find((obj) => obj.sys.id === id);
 
   const images = {
     xx99: "7eL5UvPato2ihWnBsvPBr8",
+    zx9: "66nL343lmgahxpNxpJDqiI",
+    yx1: "LaulTajbAU08vmBPkgCqS",
   };
 
-  const [xx99] = Object.values(images).map((id) => findAssetById(assets, id));
+  const [xx99, zx9, yx1] = Object.values(images).map((id) =>
+    findAssetById(assets, id)
+  );
+
+  const categories = [
+    { img: xx99.fields.file.url, title: "headphones" },
+    { img: zx9.fields.file.url, title: "speakers" },
+    { img: yx1.fields.file.url, title: "earphones" },
+  ];
 
   return (
-    <Container>
+    <Container sx={{marginTop: "1rem"}}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={4}>
-          <Card elevation={0} sx={{ position: "relative" }}>
-            <Box sx={{ position: "relative", width: "100%" }}>
-              <CardHeader sx={{ height: "80px" }}></CardHeader>
-              <CardContent
-                sx={{
-                  backgroundColor: "#f1f1f1",
-                  height: ["165px", "165px", "204px"],
-                  borderRadius: "8px",
-                }}
-              ></CardContent>
-            </Box>
-            {/* <Box sx={{ position: "absolute", height: "160px" }}> */}
-              <Box sx={{ position: "relative", height: "160px" }}>
-                <Image
-                  src={`https:${xx99.fields.file.url}`}
-                  alt=""
-                  fill
-                  style={{ objectFit: "contain" }}
-                ></Image>
-              {/* </Box> */}
-            </Box>
-          </Card>
-        </Grid>
-
-
-
-
-
-
-
-
-
-        <Grid item xs={12} sm={4}>
-          <Card elevation={0} sx={{ position: "relative" }}>
-            <Box sx={{ position: "relative", width: "100%" }}>
-              <CardHeader sx={{ height: "80px" }}></CardHeader>
-              <CardContent
-                sx={{
-                  backgroundColor: "#f1f1f1",
-                  height: ["165px", "165px", "204px"],
-                  borderRadius: "8px",
-                }}
-              ></CardContent>
-            </Box>
-            <Box sx={{ position: "absolute" }}>
-              <Box sx={{ position: "relative", height: "160px" }}>
-                <Image
-                  src={`https:${xx99.fields.file.url}`}
-                  alt=""
-                  fill
-                  style={{ objectFit: "contain" }}
-                ></Image>
-              </Box>
-            </Box>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Card elevation={0} sx={{ position: "relative" }}>
-            <Box sx={{ position: "relative", width: "100%" }}>
-              <CardHeader sx={{ height: "80px" }}></CardHeader>
-              <CardContent
-                sx={{
-                  backgroundColor: "#f1f1f1",
-                  height: ["165px", "165px", "204px"],
-                  borderRadius: "8px",
-                }}
-              ></CardContent>
-            </Box>
-            <Box sx={{ position: "absolute" }}>
-              <Box sx={{ position: "relative", height: "160px" }}>
-                <Image
-                  src={`https:${xx99.fields.file.url}`}
-                  alt=""
-                  fill
-                  style={{ objectFit: "contain" }}
-                ></Image>
-              </Box>
-            </Box>
-          </Card>
-        </Grid>
+        {categories.map((category, index) => {
+          return (
+            <Grid key={index} item xs={12} sm={4}>
+              <Card elevation={0}>
+                <CardHeader sx={{ height: "80px" }}></CardHeader>
+                <CardActionArea
+                  sx={{
+                    borderRadius: "8px",
+                    "&:hover": {
+                      "& .shop": {
+                        color: "#d87d4a",
+                      },
+                    },
+                  }}
+                >
+                  <CardContent
+                    sx={{
+                      backgroundColor: "#f1f1f1",
+                      height: ["165px", "165px", "204px"],
+                      borderRadius: "8px",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        position: "relative",
+                        height: ["120px", "120px", "150px"],
+                        top: "-80px",
+                      }}
+                    >
+                      <Image
+                        src={`https:${category.img}`}
+                        alt=""
+                        fill
+                        sizes="(min-width: 0px) 10vw"
+                        style={{ objectFit: "contain" }}
+                      ></Image>
+                    </Box>
+                    <Box
+                      sx={{
+                        position: "relative",
+                        top: ["-3.5rem", "-3.75rem", "-3.5rem"],
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "1rem",
+                      }}
+                    >
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          textTransform: "uppercase",
+                          textAlign: "center",
+                        }}
+                      >
+                        {category.title}
+                      </Typography>
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          textTransform: "uppercase",
+                          textAlign: "center",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          gap: "0.75rem",
+                        }}
+                        className="shop"
+                      >
+                        SHOP
+                        <ArrowForwardIosIcon
+                          className="arrow"
+                          fontSize="40px"
+                          sx={{ color: "#d87d4a" }}
+                        />
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          );
+        })}
       </Grid>
     </Container>
   );
