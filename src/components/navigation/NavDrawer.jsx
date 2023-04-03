53d2b9fe720bc2248e55880e7e4ba52f49e65eb1
Jesse Krensel <jdkrensel@gmail.com>
2023-04-03 19:25:02 -0400

    feat: add responsive navdrawer component with animations

---
+++ src/components/navigation/NavDrawer.jsx
@@ -0,0 +1,39 @@
+import { Box, Backdrop } from "@mui/material";
+import Gallery from "@/components/Gallery";
+import { motion, AnimatePresence } from "framer-motion";
+import { theme } from "@/styles/theme";
+
+export default function NavDrawer({ assets, onClick, isOpen }) {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <>
+          <Box
+            sx={{
+              width: "100%",
+              backgroundColor: "#ffffff",
+              padding: ["1rem 0 2rem", "3rem 0 4rem"],
+              position: "absolute",
+              zIndex: theme.zIndex.drawer,
+            }}
+            role="presentation"
+            onClick={onClick}
+            component={motion.div}
+            initial={{ y: -831 }}
+            animate={{ y: 0 }}
+            exit={{ y: -831 }}
+            transition={{ duration: 0.4 }}
+            key="modal"
+          >
+            <Gallery assets={assets}></Gallery>
+          </Box>
+          <Backdrop
+            sx={{ color: "#000", zIndex: (theme) => theme.zIndex.drawer - 1 }}
+            open={isOpen}
+            onClick={onClick}
+          />
+        </>
+      )}
+    </AnimatePresence>
+  );
+}
